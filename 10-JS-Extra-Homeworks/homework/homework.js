e393007bacff994f38f96e6ba625a475c483f6b8
Juan <juampi.parzybal@gmail.com>
2023-03-06 14:51:29 -0300

    Homework extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -64,6 +64,18 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let array = [];
+  let palabra = "";
+  let fraseArray = str.split(" ")
+  for (let i = 0; i < fraseArray.length; i++) {
+    palabra = fraseArray[i].split("")
+    palabra.reverse(" ");
+    palabra = palabra.join("")
+    array.push(palabra);
+
+  }
+  return array.join(" ")
+  
 } 
 
 
@@ -72,6 +84,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let nuevoNum = numero.toString();
+  nuevoNum = nuevoNum.split("");
+  nuevoNum.reverse();
+  nuevoNum = nuevoNum.join("")
+  if(nuevoNum == numero){
+    return "Es capicua"
+  } else {
+    return "No es capicua"
+  }
 }
 
 
